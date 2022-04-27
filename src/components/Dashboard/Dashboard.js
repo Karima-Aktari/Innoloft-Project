4bd5623fa0cecfdee90c8c047405da09646cf39f
Karima-Aktari <karimaaktari911@gmail.com>
2022-04-27 16:01:58 +0600

    nav image user

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -5,15 +5,15 @@ import { NavLink, Outlet } from 'react-router-dom';
 const Dashboard = () => {
     return (
         <div>
-            <div class="grid md:grid-cols-5 sm:grid-cols-1 gap-2">
-                <div className='bg-slate-900 text-gray-50 md:w-72 sm:w-full h-full px-4'>
-                    <div className='flex flex-col'>
+            <div class="grid md:grid-cols-5 sm:grid-cols-1 gap-4">
+                <div className='bg-slate-900 text-gray-50 md:w-72 sm:w-full h-full px-3'>
+                    <div className='flex flex-col col-span-1'>
                         <NavLink to="/" className="py-5">DashBoard</NavLink>
                         <NavLink to="/dashBoard/mainpage">Main Page</NavLink>
                         <NavLink to="/dashBoard/productpage">Product</NavLink>
                     </div>
                 </div>
-                <div className='col-span-4 bg-blue-500'>
+                <div className='col-span-4 mx-5'>
 
                     <Outlet></Outlet>
                 </div>
--- src/components/Dashboard/ProductPage/ProductPage.js
+++ src/components/Dashboard/ProductPage/ProductPage.js
@@ -1,27 +1,48 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ProductPage = () => {
-    const [products, setProducts] = useState();
+    const [product, setProduct] = useState();
 
+    //Product-data
+    useEffect(() => {
+        fetch('https://api-test.innoloft.com/product/6781/')
+            .then(res => res.json())
+            .then(data => setProduct(data))
+    }, []);
+    console.log(product);
     return (
-        <div>
-
+        <div className='container mx-auto'>
+            <h1>ProductPage</h1>
             <div className='grid md:grid-cols-3 sm:grid-cols-1 gap-2'>
                 <div className='col-span-2'>
-                    <h1>ProductPage</h1>
+                    <div className="w-96 mx-auto h-80">
+                        <img src={product.picture} className="w-full" alt='' />
+                    </div>
+                    <div className='text-center bg-blue-200 py-3'>
+                        <h1 className='text-3xl font-bold'>{product.name}</h1>
+                        <h1 className='text-xl font-bold'>{product.type.name}</h1>
+                    </div>
+                    <div className='w-full'>
+                        <button className='bg-pink-600 text-center text-xl w-1/2 text-slate-200 py-2'>Description</button>
+                        <button className='bg-violet-600 text-center text-xl w-1/2 text-slate-200 py-2'>Attributes</button>
+                    </div>
                     <div>
-                        <img src='' alt='' />
+                        Description
                     </div>
                 </div>
                 <div>
-                    <div>
+                    <div className='bg-gray-400 rounded p-3'>
                         <h1>User Info</h1>
+                        <img src={product.user.profilePicture} className="w-full rounded" alt='' />
+                        <h1 className='text-2xl font-bold'>{product.user.firstName} {product.user.lastName}</h1>
+                        <h2 className='text-xl font-bold'>{product.company.name}</h2>
                     </div>
                     <div>
                         <h1>Map</h1>
                     </div>
                 </div>
             </div>
+
         </div>
     );
 };
